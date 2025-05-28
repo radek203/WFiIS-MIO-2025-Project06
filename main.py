f8c51a98a24a6f8aeb58895044cf117629a9f84c
radek203 <r.porebinski@gmail.com>
2025-05-28 21:43:20 +0200

    Add yeast dataset and Model class

---
+++ main.py
@@ -0,0 +1,4 @@
+import yeast
+
+if __name__ == "__main__":
+    yeast.calc()
---
+++ model.py
@@ -0,0 +1,75 @@
+import numpy as np
+import tensorflow as tf
+from keras.src.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+
+from utils import augment_data
+
+
+class Model:
+
+    def __init__(self, structure, epoches):
+        self.structure = structure
+        self.epoches = epoches
+
+    def learn(self, X, y, early_stopping=-1, augment=False):
+        # Zakodowanie etykiet
+        le = LabelEncoder()
+        y_encoded = le.fit_transform(y)
+        y_cat = to_categorical(y_encoded)
+
+        # Skalowanie danych
+        scaler = StandardScaler()
+        X_scaled = scaler.fit_transform(X)
+
+        # Podział na zbiory treningowy i testowy
+        X_train, X_test, y_train, y_test = train_test_split(
+            X_scaled, y_cat, test_size=0.3, stratify=y_encoded, random_state=42
+        )
+
+        if augment:
+            X_aug = augment_data(X_train)
+            y_aug = y_train.copy()
+
+            # Łączenie danych oryginalnych i zaugmentowanych
+            X_train = np.vstack([X_train, X_aug])
+            y_train = np.vstack([y_train, y_aug])
+
+        # Budowa modelu
+        model = Sequential(self.structure)
+
+        model.compile(
+            optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy', tf.keras.metrics.F1Score(average='macro')]
+        )
+
+        # Trening
+        if early_stopping >= 1:
+            es = EarlyStopping(
+                monitor='val_loss', # 'val_accuracy', 'val_f1_score'
+                patience=early_stopping # liczba epok bez poprawy
+            )
+
+            history = model.fit(
+                X_train, y_train,
+                validation_data=(X_test, y_test),
+                epochs=self.epoches,
+                batch_size=32,
+                verbose=0,
+                callbacks=[es]
+            )
+        else:
+            history = model.fit(
+                X_train, y_train,
+                validation_data=(X_test, y_test),
+                epochs=self.epoches,
+                batch_size=32,
+                verbose=0
+            )
+
+        return history
